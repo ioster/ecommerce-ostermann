c63273404c8029a698158ef1c7af225905bc1001
i.oster <ivan.ostermann@gmail.com>
2022-07-27 22:27:49 -0300

    ItemListContainer Update

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -7,22 +7,11 @@ const ItemListContainer = ({greeting}) => {
   const [listaProductos, setListaProductos]= useState([])
   const[mensaje, setMensaje] = useState(false)
   const [loading, setLoading] = useState(true)
-  // const {greeting}= props
-  // console.log(props)
-  // console.log('me renderice soy el itemlist')
 
- 
-
-  
-  
-
-  console.log('me monte')
-
-  useEffect(()=>{
-    console.log('soy el useEffect')
+  useEffect(()=>{    
     data
     .then((res)=> setListaProductos(res))
-    .catch(()=> setMensaje('hubo un error, intente mas tarde'))
+    .catch(()=> setMensaje('error'))
     .finally(()=> setLoading(false))
   }, [])
 
@@ -30,13 +19,10 @@ const ItemListContainer = ({greeting}) => {
   return (
     <div>
       <h2>{greeting}</h2>
-      {mensaje && <p>{mensaje}</p>}
+      {mensaje && <p>{mensaje}</p>}      
       
-      {/* desafio promesa */}
        { loading ? <p>Cargando...</p>  : <ItemList listaProductos={listaProductos}/>}
 
-        {/* itemCount como el desafio del contador */}
-      {/* <ItemCount initial={1} stock={15} onAdd={onAdd} /> */}
     </div>
   )
 }
