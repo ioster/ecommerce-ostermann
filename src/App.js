4ac0a521f08d330fdc3e01e57813ba51c2b92122
i.oster <ivan.ostermann@gmail.com>
2022-08-01 21:44:15 -0300

    ItemDetail & ItemDetailContainer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <NavBar />
       <ItemListContainer textoSalida={saludar} />
+      <ItemDetailContainer />
       
     </div>
   );
